77d21fa43b0385c5fb31c17a890bdd02de39ba14
Luca Ragazzini <lucaragazzini2@gmail.com>
2026-02-12 14:55:47 +0100

    Refactor UI layout to prevent clipping and overlap

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { formatClockTime, formatDateShort } from '../utils/date';
 
 const Clock: React.FC = () => {
@@ -8,17 +8,14 @@ const Clock: React.FC = () => {
     const timer = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
     return () => clearInterval(timer);
   }, []);
 
   return (
-    <div className="flex flex-col items-end text-right z-50">
-      <div className="text-8xl font-black tracking-tighter leading-none text-slate-900">
-        {formatClockTime(time)}
-      </div>
-      <div className="text-4xl font-black text-orange-500 uppercase tracking-[0.1em] mt-3">
-        {formatDateShort(time)}
-      </div>
+    <div className="clock-panel">
+      <div className="clock-time">{formatClockTime(time)}</div>
+      <div className="clock-date">{formatDateShort(time)}</div>
     </div>
   );
 };
